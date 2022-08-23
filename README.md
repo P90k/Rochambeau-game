d3e780db9cfec117c8eb3ae49a5eb6a7de58faa5
Andreas Jansson <andreas.jansson234@gmail.com>
2022-08-23 13:45:08 +0200

    Create function game, edit index.html & README.md
    - function game: will run a rock-paper-scissors game in a best of
    five between the player and the computer. It will keep score
    and announce winner or loser when determined.
    
    - edit index.html: link script.js to index.html
    
    - edit README.md: add simple description of the project and an
    explanation to a decision within the project.

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Rochambeau-game
+
+This project is a simple game of rock-paper-scissors. 
+
+You can find out more about the rules here: 
+https://wrpsa.com/the-official-rules-of-rock-paper-scissors/
+
+I made the decision of making it best of five instead of five rounds, it made more sense to me. 
--- script.js
+++ script.js
@@ -1,22 +1,6 @@
 
-/*
+
 //Rock paper scissors game 
-
-//The client will play against the computer in three rounds. 
-
-// Create variable to store client_score, type of value is integer. Initial value is 0. 
-// Create value to store pc_score, type of value is integer. Initial value is 0. 
-
-// First to two wins the game.  (Best of three)
-
-// Check if client_score or pc_score is equal to two. If not then proceed. 
-// If client_score or pc_score is equal to two then check who won and announce that to the client. 
-
-// If no one has won then proceed with below:
-
-// Ask client for input, rock paper or scissors? 
-
-*/
 
 
 
@@ -25,7 +9,7 @@
 function getComputerChoice() {
     const rock_paper_scissors = ['rock', 'paper', 'scissors'];
     let choice = rock_paper_scissors[Math.floor(Math.random()*3)];
-    return choice
+    return choice;
 } 
 
 
@@ -34,35 +18,83 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection)  {
     // Make the arguments case insensitive.
-    playerSelection = playerSelection.toLowerCase()
-    computerSelection = computerSelection.toLowerCase()
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
 
     if (playerSelection == computerSelection) {
-        return `The computer chose ${computerSelection}. Even!`
+        return `The computer chose ${computerSelection}. Even, go again!`;
     } 
     
     else if (playerSelection == 'rock') {
         if (computerSelection == 'scissors') {
-            return `The computer chose ${computerSelection}. You won!`
+            return `The computer chose ${computerSelection}. You won!`;
         } else {
-            return `The computer chose ${computerSelection}. You lost!`
+            return `The computer chose ${computerSelection}. You lost!`;
         }
     } 
     
     else if (playerSelection == 'paper') {
         if (computerSelection == 'rock') {
-            return `The computer chose ${computerSelection}. You won!`
+            return `The computer chose ${computerSelection}. You won!`;
         } else {
-            return `The computer chose ${computerSelection}. You lost!`
+            return `The computer chose ${computerSelection}. You lost!`;
         }
     } 
     
     else if (playerSelection == 'scissors') { // use "else if" to make the code more readable
         if (computerSelection == 'paper') {
-            return `The computer chose ${computerSelection}. You won!`
+            return `The computer chose ${computerSelection}. You won!`;
         } else {
-            return `The computer chose ${computerSelection}. You lost!`
+            return `The computer chose ${computerSelection}. You lost!`;
         }
     }
 }
+
+/* 
+game function will play a rock-paper-scissors game in a best of five between the user and computer. 
+*/
+
+// Create function named game. 
+
+function game() {
+    // create variable playerScore, datatype number. Initial value 0. 
+    let playerScore = 0;
+
+    // create variable computerScore, datatype number. Initial value 0.  
+    let computerScore = 0;
+
+    // Create while loop. Condition: If playerScore or computerScore isn't 3, then continue playing the game.
+    while (playerScore < 3 && computerScore < 3) {
+
+        // Create variable to store player input, ask player for input. 
+        let playerChoice = prompt('What do you choose? rock, paper or scissors? '); // This will need to be prompted to user 
+
+        // Create variable to store value of getComputerChoice function. 
+        let computerChoice = getComputerChoice();
+
+        // Create variable to store results of round in. Call the playRound function to play a round. 
+        let result = playRound(playerChoice, computerChoice);
+
+        // Interpret results from playRound function and add score to winning player. 
+        if (result == `The computer chose ${computerChoice}. Even, go again!`) {
+            console.log(result);
+        } else if (result == `The computer chose ${computerChoice}. You won!`) {
+            console.log(result);
+            playerScore++;
+        } else if (result == `The computer chose ${computerChoice}. You lost!`) {
+            console.log(result);
+            computerScore++
+        }
+    }
+
+    // Determine who won the game by conditional statement. 
+    if (playerScore == 3) {
+        console.log('Congratulations, you won the game!')
+    } else {
+        console.log('GAME OVER')
+    }
+}
+
+game()
+
 
